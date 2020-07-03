77b30224c3f58f578579c75c3e74d3facc2b70fb
Shradha Seth <66904280+ss1998-seth@users.noreply.github.com>
2020-07-03 13:27:41 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+
+
+const App= () => {
+
+let newTime =new Date().toLocaleTimeString();
+
+const [ctime,setctime] =useState(newTime);
+
+const updateTime=()=>
+{
+  newTime=new Date().toLocaleTimeString(); 
+  setctime(newTime);
+}
+setInterval(updateTime,1);
+  return (
+<>
+<h1>{ctime}</h1>
+<button onClick={updateTime}>Get time</button>
+</>
+
+  );
+}
+
+  export default App;
